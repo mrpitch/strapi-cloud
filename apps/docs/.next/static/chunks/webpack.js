d3f94793595782f384744551f9e3b235d7ecdd68
mrpitch <mrpitch@outlook.com>
2023-01-13 12:42:19 +0100

    fix: configure packages json to run with yarn

--- apps/docs/.next/static/chunks/webpack.js
+++ apps/docs/.next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "45e4ed6f2a3ff90c"; }
+/******/ 		__webpack_require__.h = function() { return "4a360ffe765955f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
