178a35fa57272c72ffb796c92cd1f82f97176656
mrpitch <mrpitch@outlook.com>
2023-01-13 16:40:28 +0100

    feat: add golf-pwa db, remove docs app

--- apps/strapi-cloud-test/config/database.ts
+++ apps/strapi-cloud-test/config/database.ts
@@ -1,13 +1,30 @@
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_cloud"),
-      user: env("DATABASE_USERNAME", "strapi_cloud"),
-      password: env("DATABASE_PASSWORD", "strapi_cloud"),
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-  },
-});
+import path from 'path'
+
+// export default ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+	connection: {
+		client: 'postgres',
+		connection: {
+			host: env('DATABASE_HOST', 'localhost'),
+			port: env.int('DATABASE_PORT', 5432),
+			database: env('DATABASE_NAME'),
+			user: env('DATABASE_USERNAME'),
+			password: env('DATABASE_PASSWORD'),
+			// schema: env('DATABASE_SCHEMA', 'public'), // Not required
+			// ssl: {
+			// 	rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+			// },
+		},
+		debug: false,
+	},
+})
--- apps/strapi-cloud-test/src/index.ts
+++ apps/strapi-cloud-test/src/index.ts
@@ -1,18 +1,65 @@
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+	/**
+	 * An asynchronous register function that runs before
+	 * your application is initialized.
+	 *
+	 * This gives you an opportunity to extend code.
+	 */
+	//register(/*{ strapi }*/) {},
+	register({ strapi }) {
+		const extensionService = strapi.service('plugin::graphql.extension')
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
-};
+		extensionService.use(({ strapi }) => ({
+			typeDefs: `
+			type Query {
+				exercise(slug: String!): ExerciseEntityResponse
+				workout(slug: String!): WorkoutEntityResponse
+			}`,
+			resolvers: {
+				Query: {
+					exercise: {
+						resolve: async (parent, args, context) => {
+							const { toEntityResponse } = strapi.service(
+								'plugin::graphql.format'
+							).returnTypes
+
+							const data = await strapi.services['api::exercise.exercise'].find(
+								{
+									filters: { slug: args.slug },
+								}
+							)
+
+							const response = toEntityResponse(data.results[0])
+
+							return response
+						},
+					},
+					workout: {
+						resolve: async (parent, args, context) => {
+							const { toEntityResponse } = strapi.service(
+								'plugin::graphql.format'
+							).returnTypes
+
+							const data = await strapi.services['api::workout.workout'].find({
+								filters: { slug: args.slug },
+							})
+
+							const response = toEntityResponse(data.results[0])
+
+							return response
+						},
+					},
+				},
+			},
+		}))
+	},
+
+	/**
+	 * An asynchronous bootstrap function that runs before
+	 * your application gets started.
+	 *
+	 * This gives you an opportunity to set up your data model,
+	 * run jobs, or perform some special logic.
+	 */
+	bootstrap(/*{ strapi }*/) {},
+}
